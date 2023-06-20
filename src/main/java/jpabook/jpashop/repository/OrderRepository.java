86777d7499cfa9f9c85c6707e8095a9b24e7bc0e
rnrudejr9 <rnrudejr1234@naver.com>
2023-06-20 17:52:22 +0900

    feat : 상품조회 기능 개발, 추후 queryDSL 리펙토링

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -20,5 +20,37 @@ public class OrderRepository {
         return entityManager.find(Order.class,id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch){}
+    public List<Order> findAll(OrderSearch orderSearch){
+        //이름이 있을때없을떄, 상태가 있을때없을때 어떻게 동적쿼리를 만들것이냐
+
+
+        return entityManager.createQuery("select o from Order o join o.member m" +
+                        " where o.status = :status and m.name like :name"
+                , Order.class)
+                .setParameter("status",orderSearch.getOrderStatus())
+                .setParameter("name",orderSearch.getMemberName())
+                .setFirstResult(0)
+                .setMaxResults(1000)
+                .getResultList();
+    }
+
+    /**
+     * JPA Criteria
+     *
+     * @param orderSearch
+     * @return
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch){
+        //이름이 있을때없을떄, 상태가 있을때없을때 어떻게 동적쿼리를 만들것이냐
+
+
+        return entityManager.createQuery("select o from Order o join o.member m" +
+                                " where o.status = :status and m.name like :name"
+                        , Order.class)
+                .setParameter("status",orderSearch.getOrderStatus())
+                .setParameter("name",orderSearch.getMemberName())
+                .setFirstResult(0)
+                .setMaxResults(1000)
+                .getResultList();
+    }
 }
