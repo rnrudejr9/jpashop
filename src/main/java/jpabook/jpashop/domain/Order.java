f3856abf5514dbde96160342bf93884d91ccf361
rnrudejr9 <rnrudejr1234@naver.com>
2023-06-20 16:02:13 +0900

    feat : 주문, 상품 엔티티 / 비즈니스로직, 생성메서드 반영

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.List;
 @Entity
 @Table(name = "orders")
 @Getter
+@Setter
 public class Order {
     @Id @GeneratedValue
     @Column(name = "order_id")
@@ -21,6 +23,7 @@ public class Order {
     // 연관관계의 주인은 누구냐? -> jpa는 둘중에 어디서 update를 쳐야되냐
     // 둘중에 하나를 주인으로 잡고 변경하면 됨 (Order가 주인)
     private Member member;
+
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     // OrderItem 안의 order
@@ -52,6 +55,46 @@ public class Order {
     public void setDelivery(Delivery delivery){
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    /**
+     * 생성 메서드
+     * 응집해둔것이라 주문과 관계된 비즈니스는 여기서 보면됨
+     */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    /**
+     * 비즈니스 로직
+     * 주문취소
+     */
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송된 상품입니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    /**
+     * 조회로직
+     * ctrl + art + n : 변수 정리
+     */
+    public int getTotalPrice(){
+        return orderItems.stream()
+                .mapToInt(OrderItem::getOrderTotalPrice)
+                .sum();
     }
 
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -23,4 +23,32 @@ public class OrderItem {
     private Order order;
     private int orderPrice; //주문가격
     private int count; //주문수량
+
+    /**
+     * 생성메서드
+     */
+    public static OrderItem createOrderItem(Item item,int orderPrice,int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    /**
+     * 주문취소
+     */
+    public void cancel(){
+        getItem().addStock(count);
+    }
+
+
+    /**
+     * 전체가격조회
+     */
+    public int getOrderTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
