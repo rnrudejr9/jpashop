5f7e6684d9fbca6cc900defafc526042423ddb1e
rnrudejr9 <rnrudejr1234@naver.com>
2023-07-15 11:14:51 +0900

    feat : 주문조회 v2, 페치조인 한계

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -39,6 +39,12 @@ public class OrderApiController {
         List<OrderDto> collect = orders.stream().map(o -> new OrderDto(o)).collect(Collectors.toList());
         return collect;
     }
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3(){
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> collect = orders.stream().map(o -> new OrderDto(o)).collect(Collectors.toList());
+        return collect;
+    }
 
     @Getter
     static class OrderDto{
